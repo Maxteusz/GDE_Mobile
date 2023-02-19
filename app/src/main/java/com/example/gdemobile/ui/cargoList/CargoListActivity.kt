8292ac0941608bf80d0c26f3a09de9eb8e873e39
Mateusz Chabiński <maxteusz@gmail.com>
2023-02-19 16:45:18 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/gdemobile/ui/cargoList/CargoListActivity.kt
+++ app/src/main/java/com/example/gdemobile/ui/cargoList/CargoListActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gdemobile.databinding.ActivityCargoListBinding
@@ -26,6 +27,9 @@ class CargoListActivity : AppCompatActivity() {
         adapter = ContractorAdapter(CargoListView.listContractor())
         binding.recyclerview.adapter = adapter
         binding.recyclerview.setHasFixedSize(true)
+        Log.i("Focus", this.currentFocus.toString())
+
+        Toast.makeText(applicationContext, "dsdsd", Toast.LENGTH_SHORT).show()
 
 
         binding.cameraButton.setOnClickListener({
@@ -33,27 +37,32 @@ class CargoListActivity : AppCompatActivity() {
         })
 
         var temp = "";
-        binding.recyclerview.setOnKeyListener { _, keyCode, event ->
+        /*binding.recyclerview.setOnKeyListener { _, keyCode, event ->
+            binding.linearLayout2.requestFocus()
+            Log.i("FocusLog", this.currentFocus.toString())
             if(event.action == KeyEvent.ACTION_UP) {
                 temp += event.getUnicodeChar().toChar()
-                Log.i("Chars", temp)
+                binding.barcodeEdittext.setText(temp)
+               Toast.makeText(this, temp, Toast.LENGTH_SHORT)
+
             }
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
                 Log.i("Key", "Pressed")
-               /* binding.barcodeEdittext.requestFocusFromTouch()
+               *//* binding.barcodeEdittext.requestFocusFromTouch()
                 binding.barcodeEdittext.requestFocus()
-                binding.barcodeTextfield.requestFocus()*/
+                binding.barcodeTextfield.requestFocus()*//*
 
 
-                //binding.barcodeEdittext.setText("dsdsdsfff")
+                binding.barcodeEdittext.setText("dsdsdsfff")
 
                 return@setOnKeyListener true
             }
-            false
+            false*/
 
-        }
+        //}
 
         //Keep focus on barcodeEditText and hide keyboard
+
 
 
 
@@ -61,10 +70,22 @@ class CargoListActivity : AppCompatActivity() {
        // pobierzKontrahentow()
 
     }
+var temp = ""
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+       // Toast.makeText(applicationContext, "dsdsd", Toast.LENGTH_SHORT)
+        temp += event.getUnicodeChar().toChar()
+        return when (keyCode) {
+            KeyEvent.KEYCODE_ENTER -> {
+               Toast.makeText(applicationContext, temp, Toast.LENGTH_SHORT).show()
+                temp ="";
+                true
+            }
+            else -> super.onKeyUp(keyCode, event)
+        }
+    }
 
 
-
-  /*  fun pobierzKontrahentow() {
+/*  fun pobierzKontrahentow() {
 
         var retrofit = RetrofitClient().getInstance()
         var apiInterface = retrofit.create(ApiInterface::class.java)
