ff54b3214306229b0e6461deb48a059385cf1b12
Mateusz <maxteusz@gmail.com>
2023-09-17 22:34:41 +0200

    Make GetDocumentPositions from Api

--- app/src/main/java/com/example/gdemobile/apiConnect/enovaConnect/methods/GetDocumentPositionsOnDocument.kt
+++ app/src/main/java/com/example/gdemobile/apiConnect/enovaConnect/methods/GetDocumentPositionsOnDocument.kt
@@ -4,10 +4,10 @@ import com.example.gdemobile.apiConnect.enovaConnect.BaseDto
 import com.example.gdemobile.apiConnect.enovaConnect.IDto
 import com.google.gson.annotations.SerializedName
 
-class GetDocumentPositionsOnDocument(val idDocument: String = "1c157a9e-9b1b-435b-b7c8-e3a90e3e1919") :
+class GetDocumentPositionsOnDocument(val idDocument: String) :
     IConnectEnovaMethod {
     override val methodName: String
-        get() = "PobierzPozycjeDokumentuPrzyjeciaWewnetrznegoMagazynowego"
+        get() = "PobierzPozycjeDokumentuPrzyjeciaZewnetrznegoMagazynowego"
     override val methodService: String
         get() = "GdeApi.IDokumentyPrzyjecMagazynowychService, GdeApi"
     override val dto: IDto
