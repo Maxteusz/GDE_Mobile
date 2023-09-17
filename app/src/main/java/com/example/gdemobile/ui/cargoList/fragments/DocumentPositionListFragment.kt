9e55981bba76cec02197a598d877f80be506572e
Mateusz <maxteusz@gmail.com>
2023-09-17 22:53:04 +0200

    Make GetDocumentPositions from Api

--- app/src/main/java/com/example/gdemobile/ui/cargoList/fragments/DocumentPositionListFragment.kt
+++ app/src/main/java/com/example/gdemobile/ui/cargoList/fragments/DocumentPositionListFragment.kt
@@ -52,6 +52,8 @@ class DocumentPositionListFragment() : Fragment(), StateResponse, KeyListener {
                     Log.i("Get Parametr", args)
                     viewModel.getDocumentPositions(args)
                 }
+                else
+                    OnError()
             }
         }
         viewModel.documentPositions.observe(viewLifecycleOwner, {
