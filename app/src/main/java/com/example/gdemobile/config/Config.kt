aa2be689f49052c2904044631a3044cb751b2909
Mateusz Chabiński <maxteusz@gmail.com>
2024-05-13 14:47:33 +0200

    Add Trimming to config properties

--- app/src/main/java/com/example/gdemobile/config/Config.kt
+++ app/src/main/java/com/example/gdemobile/config/Config.kt
@@ -13,7 +13,7 @@ class Config {
         var usernameERP: String? = "Administrator"
         var passwordERP: String? = "12345"
         var fastAddingDocumentPosition: Boolean = false
-        var insertAmountCargo : Boolean = false;
+
 
         fun saveConfiguration(context: Context) {
 
@@ -22,14 +22,12 @@ class Config {
                 Context.MODE_PRIVATE
             ) ?: return
             with(sharedPref.edit()) {
-                putString(context.getString(R.string.ip_conf), ip)
-                putString(context.getString(R.string.port_conf), port)
-                putString(context.getString(R.string.database_conf), database)
-                putString(context.getString(R.string.username_conf), usernameERP)
-                putString(context.getString(R.string.password_conf), passwordERP)
+                putString(context.getString(R.string.ip_conf), ip?.trim())
+                putString(context.getString(R.string.port_conf), port?.trim())
+                putString(context.getString(R.string.database_conf), database?.trim())
+                putString(context.getString(R.string.username_conf), usernameERP?.trim())
+                putString(context.getString(R.string.password_conf), passwordERP?.trim())
                 putBoolean(context.getString(R.string.fast_adding_documentpostion), fastAddingDocumentPosition)
-                putBoolean(context.getString(R.string.insert_amount_cargo), insertAmountCargo)
-
                 apply()
                 commit()
             }
@@ -46,7 +44,7 @@ class Config {
             usernameERP = sharedPref.getString(context.getString(R.string.username_conf), "")
             passwordERP = sharedPref.getString(context.getString(R.string.password_conf), "")
             fastAddingDocumentPosition = sharedPref.getBoolean(context.getString(R.string.fast_adding_documentpostion), true)
-            insertAmountCargo = sharedPref.getBoolean(context.getString(R.string.insert_amount_cargo), false)
+
         }
 
 
