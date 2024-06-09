ed6a3cff71eb03a53620fed4153d82c678337d47
Mateusz <maxteusz@gmail.com>
2024-06-09 21:05:57 +0200

    Working on UpdateApp

--- app/src/main/java/com/example/gdemobile/config/UpdateApp.kt
+++ app/src/main/java/com/example/gdemobile/config/UpdateApp.kt
@@ -1,8 +1,12 @@
 package com.example.gdemobile.config
 
+import android.content.Context
+import android.os.Build
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Base64
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.gdemobile.apiConnect.enovaConnect.daos.Dao
@@ -11,15 +15,19 @@ import com.example.gdemobile.apiConnect.enovaConnect.methods.interfaces.IDto
 import com.example.gdemobile.ui.IStateResponse
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.http.PATCH
 import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.io.path.Path
+import kotlin.io.path.createFile
 
 class UpdateApp(private val fragment: Fragment) {
 
-
+val context : Context? = fragment.context
     private val updateStateResponse: IStateResponse = object : IStateResponse {
         override fun onLoading() {
 
@@ -27,41 +35,49 @@ class UpdateApp(private val fragment: Fragment) {
 
         override suspend fun onError(message: String) {
 
+
         }
 
         override fun onSuccess() {
 
+
         }
 
     }
-    suspend fun saveFile()
-    {
 
-        var pathh = Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_DOWNLOADS);
-        val file = convertToFile()
+
+
+    suspend fun saveFile() {
+
+        val file = convertToFile().await()
+        val filePath = Paths.get(context?.getExternalFilesDir(null).toString() + "/test.apk")
+
         withContext(Dispatchers.IO) {
-            Files.write(pathh, file)
+            Files.createFile(filePath)
+            if (!Files.exists(filePath))
+                Files.createFile(filePath)
+            Files.write(filePath, file)
         }
+
+
+
 
     }
 
-    private suspend fun convertToFile(): ByteArray {
 
-        fragment.viewLifecycleOwner.lifecycleScope.launch {
+    private suspend fun convertToFile(): Deferred<ByteArray> {
+
+        val deferred = fragment.lifecycleScope.async(Dispatchers.IO) {
             val text = downloadFile()
-            if (text != null) {
-                Log.i("Pobrany Text", text)
-                Base64.decode(text, Base64.DEFAULT)
-            }
+            val decodedBytes = Base64.decode(text!!, Base64.DEFAULT)
+            return@async decodedBytes
         }
-        return ByteArray(0)
-
+        return deferred
     }
 
-    private suspend fun downloadFile() : String? {
-            return UpdateDao(updateStateResponse).downloadFileInString("dd")
 
+    private suspend fun downloadFile(): String? {
+        return UpdateDao(updateStateResponse).downloadFileInString("dd")
 
 
     }
