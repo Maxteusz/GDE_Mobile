636dc84680816e568b194f2a545f1deda2b2ea1a
Mateusz Chabiński <maxteusz@gmail.com>
2024-06-09 12:49:31 +0200

    Working on Update App

--- app/src/main/java/com/example/gdemobile/config/UpdateApp.kt
+++ app/src/main/java/com/example/gdemobile/config/UpdateApp.kt
@@ -1,101 +1,79 @@
 package com.example.gdemobile.config
 
-import android.app.Activity
-import android.app.DownloadManager
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Environment
 import android.util.Base64
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.content.ContextCompat.startActivity
-import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.gdemobile.apiConnect.enovaConnect.daos.Dao
 import com.example.gdemobile.apiConnect.enovaConnect.methods.interfaces.IConnectEnovaMethod
 import com.example.gdemobile.apiConnect.enovaConnect.methods.interfaces.IDto
 import com.example.gdemobile.ui.IStateResponse
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.nio.file.Files
+import kotlin.io.path.Path
 
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.jar.Manifest
-
-class UpdateApp(private val context: Context)   {
+class UpdateApp(private val fragment: Fragment) {
 
 
-
-    private suspend fun downloadFileInString(test: String) : String? {
-        return requestObject<String>(UpdateAppConnection())
-
-    }
-
-    private val updateStateResponse  = object : IStateResponse
-    {
+    private val updateStateResponse: IStateResponse = object : IStateResponse {
         override fun onLoading() {
-            TODO("Not yet implemented")
+
         }
 
         override suspend fun onError(message: String) {
-            TODO("Not yet implemented")
+
         }
 
         override fun onSuccess() {
-            TODO("Not yet implemented")
+
         }
 
     }
-    fun downloadFile() {
-        try {
-            val uri = Uri.parse("https://pdfobject.com/pdf/sample.pdf");
-            Base64.decode("ddd", Base64.DEFAULT)
-            val request = DownloadManager.Request(uri);
-            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);  // Tell on which network you want to download file.
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);  // This will show notification on top when downloading the file.
-            request.setTitle("Pobieranie pliku instalacyjnego..."); // Title for notification.
-            request.setVisibleInDownloadsUi(true);
-
-            request.setDestinationInExternalFilesDir(
-                context,
-                Environment.getExternalStorageState(),
-                "fff" + ".pdf"
-            );
-            val downloadManager =
-                context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            val file = downloadManager.enqueue(request)
-            downloadManager.openDownloadedFile(downloadManager.enqueue(request))
-            Log.i(
-                "Download Manager",
-                "Download started" + downloadManager.getUriForDownloadedFile(file)
-            )
-            // This will start downloading
-        } catch (_: Exception) {
-
-        }
-    }
-
-    private fun installApk(file: File) {
-        val apkUri = FileProvider.getUriForFile(
-            context,
-            "${context.packageName}.provider",
-            file
-        )
-        val intent = Intent(Intent.ACTION_VIEW).apply {
-            setDataAndType(apkUri, "application/vnd.android.package-archive")
-            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        }
-        context.startActivity(intent)
-    }
-
-    private class UpdateDao(stateResult : IStateResponse) : Dao(stateResult)
+    suspend fun saveFile()
     {
 
+        var pathh = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_DOWNLOADS);
+        val file = convertToFile()
+        withContext(Dispatchers.IO) {
+            Files.write(pathh, file)
+        }
+
     }
+
+    private suspend fun convertToFile(): ByteArray {
+
+        fragment.viewLifecycleOwner.lifecycleScope.launch {
+            val text = downloadFile()
+            if (text != null) {
+                Log.i("Pobrany Text", text)
+                Base64.decode(text, Base64.DEFAULT)
+            }
+        }
+        return ByteArray(0)
+
+    }
+
+    private suspend fun downloadFile() : String? {
+            return UpdateDao(updateStateResponse).downloadFileInString("dd")
+
+
+
+    }
+
+
+    private class UpdateDao(stateResult: IStateResponse) : Dao(stateResult) {
+        suspend fun downloadFileInString(test: String): String? {
+            return requestObject<String>(UpdateAppConnection())
+
+        }
+    }
+
     private class UpdateAppConnection : IConnectEnovaMethod {
         override val methodName: String
             get() = "PobierzPlikApk"
--- app/src/main/java/com/example/gdemobile/ui/fragments/MenuFragment.kt
+++ app/src/main/java/com/example/gdemobile/ui/fragments/MenuFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.gdemobile.R
 import com.example.gdemobile.apiConnect.enovaConnect.helpers.documenttypes.Acceptance
@@ -16,6 +17,7 @@ import com.example.gdemobile.apiConnect.enovaConnect.helpers.documenttypes.Issua
 import com.example.gdemobile.config.UpdateApp
 import com.example.gdemobile.databinding.FragmentMenuBinding
 import com.example.gdemobile.ui.viewmodels.SharedViewModel
+import kotlinx.coroutines.launch
 
 
 class MenuFragment : Fragment() {
@@ -48,8 +50,12 @@ class MenuFragment : Fragment() {
             findNavController().navigate(R.id.action_menuFragment_to_documentListFragment)
         }
         binding.logoImage.setOnClickListener {
-            val updateManager = UpdateApp(requireActivity());
-            updateManager.downloadFile()
+            viewLifecycleOwner.lifecycleScope.launch {
+
+                val updateManager = UpdateApp(this@MenuFragment).saveFile()
+            }
+
+
 
         }
     }
